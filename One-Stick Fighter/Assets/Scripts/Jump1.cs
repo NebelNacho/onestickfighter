47077bf5032e974ea031c328903f7d5b7b2f294f
NebelNacho (@home) <maltemon@web.de>
2018-11-12 18:36:52 +0100

    uploaded basic project files
    the basic Unity Project Folder. contains all needed files
    also contains some first scripts and scenes, which sketch the games feel

---
+++ One-Stick Fighter/Assets/Scripts/Jump1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump1 : MonoBehaviour {
+    private Rigidbody rb;
+    public float jumpstrenght = 10;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(key: KeyCode.UpArrow))
+        {
+            rb.AddForce(Vector3.up * jumpstrenght, ForceMode.Impulse);
+        }
+        if(Input.GetKey(key: KeyCode.LeftArrow))
+        {
+            // rb.AddForce(force: Vector3.left * jumpstrenght * 1, mode: ForceMode.VelocityChange);
+            transform.Translate(translation: Vector3.left / 5);
+        }
+        if (Input.GetKeyDown(key: KeyCode.DownArrow))
+        {
+            rb.AddForce(force: Vector3.down * jumpstrenght * 3, mode: ForceMode.Impulse);
+        }
+        if (Input.GetKey(key: KeyCode.RightArrow))
+        {
+            //  rb.AddForce(force: Vector3.right * jumpstrenght * 1, mode: ForceMode.VelocityChange);
+            transform.Translate(translation: Vector3.right / 5);
+        }
+    }
+}
